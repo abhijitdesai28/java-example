3d59d82d9b332079d74a5bad3c1fa0f12fd82ae1
unknown <desaiabhijit28@gmail.com>
2024-01-28 21:39:58 +0530

    serialization Demonstrate

--- files/src/FileDemo.java
+++ files/src/FileDemo.java
@@ -1,5 +1,16 @@
-public class Main {
+public class FileDemo {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // string block content declaration in till Java-11
+        String content = "hello world, this is sample content that actually needs to be "
+                + "written in File using Java-11 new File API.\nLet's perform this activity.";
+
+        // string block content declaration in Java-17
+        content = """
+                hello world, this is sample content that actually needs to be written in File using Java-11 new File API.
+                Let's perform this activity.
+                """;
+
+        //String tempFolder = "/home/kodtodya/Downloads/fileDemo";
+        String tempFolder = "C:\\Users\\Abhijit Desai\\Downloads\\fileDemo\\";
     }
 }
