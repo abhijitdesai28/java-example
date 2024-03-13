2a35bae0eb093766e01ea3d2116b70af470a7819
unknown <desaiabhijit28@gmail.com>
2024-03-13 23:42:06 +0530

    program

--- collectionFramwork/src/set/TreeSetDemo1.java
+++ collectionFramwork/src/set/TreeSetDemo1.java
@@ -47,6 +47,45 @@ public class TreeSetDemo1 {
         
         treeSetObject3.addAll(treeSetObject4);
 
-        System.out.println("Values in TreeSet object 3: " +treeSetObject3);
+        System.out.println("Values in TreeSet object 3: " +treeSetObject3);//[1, 2, 3, 5, 7, 8, 9, 11]
+
+//        3. Object ceiling(Object o):
+//        This method retrieves the least element which is greater than or equal to the given element in the argument list,
+//        or null if there is no such element in the set
+        System.out.println("Ceiling value of '5' is:" +treeSetObject3.ceiling(5));//5
+
+        System.out.println("Ceiling value of '2' is:" +treeSetObject3.ceiling(4));//5
+
+//        4. void clear():
+//        This method removes all of the elements from this object
+        System.out.println("Values in TreeSet object 1:" +treeSetObject1);//[1, 2, 3, 5]
+        treeSetObject1.clear();
+        System.out.println("Values in TreeSet object 1:" +treeSetObject1);//[]
+
+//        5. Object clone():
+//        This method returns a copy of set
+        TreeSet <Integer>treeSetObject5 = new TreeSet<Integer>();
+
+        TreeSet <Integer>treeSetObject6 = new TreeSet<Integer>();
+
+        treeSetObject5.add(3);
+        treeSetObject5.add(5);
+        treeSetObject5.add(5);
+        treeSetObject5.add(1);
+        treeSetObject5.add(2);
+        System.out.println("Values in TreeSet5:" +treeSetObject5);//[1, 2, 3, 5]
+        treeSetObject6=(TreeSet<Integer>) treeSetObject5.clone();
+        System.out.println("Values in Cloned TreeSet:" +treeSetObject6);//[1, 2, 3, 5]
+
+//        7. boolean contains(Object o):
+//        This method returns true if specified element is present in the given set,
+
+        System.out.println("Checking if Tree Set Object contains element '0' : "
+
+                +treeSetObject6.contains(0));//false
+
+        System.out.println("Checking if Tree Set Object contains element '1' : "
+
+                +treeSetObject6.contains(1));//true
     }
 }
