0b9756715fee9bf8d1038c6adae7e401424286d3
unknown <desaiabhijit28@gmail.com>
2024-06-24 18:47:11 +0530

    print statements in java

---
+++ basicsOfJava/src/PrintingStatement/One.java
@@ -0,0 +1,25 @@
+package PrintingStatement;
+
+public class One
+{
+    public static void main(String[] args)
+    {
+        //System.out.print();  CTE -Should not be empty
+        System.out.print("hi");
+        System.out.println("hello");
+        System.out.print("how");
+        System.out.println();
+        //System.out.print(hi); CTE
+        System.out.println("are");
+        System.out.println("you");
+    }
+}
+
+/*
+    OUTPUT
+
+    hihello
+    how
+    are
+    you
+ */
