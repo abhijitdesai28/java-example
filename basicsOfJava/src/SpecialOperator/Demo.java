919934678c41f04c4d991966b96e6a09f89c25a4
unknown <desaiabhijit28@gmail.com>
2024-06-24 18:59:16 +0530

    special operator in java

---
+++ basicsOfJava/src/SpecialOperator/Demo.java
@@ -0,0 +1,9 @@
+package SpecialOperator;
+/*
+   in java '+' is act as special operator
+ */
+public class Demo {
+    public static void main(String[] args){
+        System.out.println(10+10+20+10+"hi"+10+20);//50hi1020
+    }
+}
