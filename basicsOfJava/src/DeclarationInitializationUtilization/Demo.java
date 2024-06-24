184db1d216b0a71955ef3906d2978dd2dcd11da4
unknown <desaiabhijit28@gmail.com>
2024-06-24 19:07:47 +0530

    Declaration/initalization/utilization

---
+++ basicsOfJava/src/DeclarationInitializationUtilization/Demo.java
@@ -0,0 +1,19 @@
+package DeclarationInitializationUtilization;
+
+public class Demo {
+    public static void main(String[] args)
+    {
+        //declaration :its the process of declaring memory block to store the value
+        int a;
+        int b;
+
+        //initialization:its the process of storing the value inside declared memory blocks
+        a=20;
+        b=30;
+
+        //utilization :its the process of utilizing the initialized value through variable
+        System.out.print(a);
+        System.out.print(b);
+
+    }
+}
