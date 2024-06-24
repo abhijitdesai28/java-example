317a13551f0befd5c75a6b22740dd965f740a1fe
unknown <desaiabhijit28@gmail.com>
2024-06-24 19:55:11 +0530

    data types

---
+++ basicsOfJava/src/DataTypes/Demo.java
@@ -0,0 +1,21 @@
+package DataTypes;
+
+public class Demo {
+    public static void main(String[] args){
+        //Data Type:data types represent different value that has to be stored in the variable
+        //data types are classified int two types
+        //primitive data types: byte/short/int/long/float/double/char/boolean
+        //non-primitive data type: class/string/arrays
+
+        byte b=127;
+        short s=1234;
+        int i=66767;
+        long l= 54656554553L;
+        float f=3.142F;
+        double d=658428.5842;
+        char c='a';
+        boolean b1=true;
+
+        System.out.println(b+ " "+ s+ " "+i+ " "+l+" "+f+" "+d+" "+c+" "+b1);
+    }
+}
