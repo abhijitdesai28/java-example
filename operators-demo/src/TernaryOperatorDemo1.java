d15ff75d74d055b445e759ddd439a747ef644fe4
unknown <desaiabhijit28@gmail.com>
2024-06-25 20:32:17 +0530

    operator example

---
+++ operators-demo/src/TernaryOperatorDemo1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TernaryOperatorDemo1
+{
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=20;
+        String s=(a>b)?"YES" : "NO";
+        System.out.println(s);
+    }
+}
